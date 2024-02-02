7c99937862cc626f9506ed3541b07441ee99f125
Andrii Varlanovych <andrewvarlanovich@gmail.com>
2024-02-02 16:18:52 +0200

    first_commit: correctly works with frontend

--- library_bend/src/main/java/au/andrii/library_bend/models/User.java
+++ library_bend/src/main/java/au/andrii/library_bend/models/User.java
@@ -1,7 +1,13 @@
 package au.andrii.library_bend.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
